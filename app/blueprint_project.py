9459b8d7ba061b9e314e92aafcb0bff4d7a9c42e
xgoods <goods.brian@gmail.com>
2017-04-23 07:36:20 -0400

    tink its working

--- app/blueprint_project.py
+++ app/blueprint_project.py
@@ -11,37 +11,40 @@ import string, random
 import requests
 from .crawl import getArticle
 import datetime, calendar, time
+
 logr = logging.getLogger('gimphub.blueprint_chan')
 project_B = Blueprint('project', __name__)
 from gridfs import GridFS
 from .XCF import XCF
+from picture_change import getChanges
+
 
 def get_gridfs():
     db = get_db()
     gf = GridFS(db)
     return gf
 
-@project_B.route('/newProject', methods = ['POST'])
+
+@project_B.route('/newProject', methods=['POST'])
 def newProject():
-    #room=request.args['room'] if 'room' in request.args else None
+    # room=request.args['room'] if 'room' in request.args else None
 
     if not 'user' in session:
-        return jsonify({'ok':0, 'err':'Must log in to create project'})
-
+        return jsonify({'ok': 0, 'err': 'Must log in to create project'})
 
     db = get_db()
 
-    #user = db.users.find_one({'_id':session['user']})
+    # user = db.users.find_one({'_id':session['user']})
 
 
 
-    db.users.update({'_id':session['user']}, {'$addToSet':{'projects':[request.json['repoName']]}})
-    db.projects.insert({'_id':"%s%s" % (session['user'], request.json['repoName']),'images':[]})
+    db.users.update({'_id': session['user']}, {'$addToSet': {'projects': [request.json['repoName']]}})
+    db.projects.insert({'_id': "%s%s" % (session['user'], request.json['repoName']), 'images': []})
 
     return jsonify({'ok': 1})
 
 
-@project_B.route('/<user>/<project>/getLiveImage', methods = ['GET'])
+@project_B.route('/<user>/<project>/getLiveImage', methods=['GET'])
 def getLiveImage(user, project):
     db = get_db()
     GFS = get_gridfs()
@@ -59,54 +62,57 @@ def getLiveImage(user, project):
         return response
     return ""
 
-@project_B.route('/getHistory', methods = ['POST'])
+
+@project_B.route('/getHistory', methods=['POST'])
 def getHistory():
     db = get_db()
     GFS = get_gridfs()
     images = db.projects.find_one({'_id': "%s%s" % (request.json['user'], request.json['project'])},
-                                 {'images': {'$slice': -10}})
+                                  {'images': {'$slice': -10}})
     if images and 'images' in images and images['images']:
+        pngs = []
         for image in images['images']:
             img = GFS.get(image)
             XC = XCF.XCF()
             XC.load_image(img)
             f, l = XC.get_full_image_and_layers()
+            pngs.append(f)
+
+        finalOut = []
+        for i, png in enumerate(pngs):
+            if i < len(pngs) - 2:
+                d, a, r = getChanges(png, pngs[i + 1])
+                finalOut.append({'d': d, 'a': a, 'r': r})
+        return jsonify({'ok': 1, 'something': finalOut})
 
 
-
-
-
-@project_B.route('/uploadImage', methods = ['POST', 'GET'])
+@project_B.route('/uploadImage', methods=['POST', 'GET'])
 def uploadImage():
-    #room=request.args['room'] if 'room' in request.args else None
+    # room=request.args['room'] if 'room' in request.args else None
     print(request.form)
     print(request.files)
 
     if not request.files:
-        return jsonify({'ok':0, 'err':'No file was selected'})
+        return jsonify({'ok': 0, 'err': 'No file was selected'})
 
     imgFile = request.files['uploadFile']
 
     print(imgFile)
 
-    GFS =get_gridfs()
+    GFS = get_gridfs()
     fileid = GFS.put(imgFile.read())
     db = get_db()
-    db.projects.update({'_id':"%s%s" % (session['user'], request.form['projectName'])}, {'$push':{'images':fileid}})
-
+    db.projects.update({'_id': "%s%s" % (session['user'], request.form['projectName'])}, {'$push': {'images': fileid}})
 
     return redirect("/%s/%s" % (session['user'], request.form['projectName']))
 
 
-
-
-
-@project_B.route('/<user>/<project>', methods = ['GET'])
+@project_B.route('/<user>/<project>', methods=['GET'])
 def project(user, project):
-    #room=request.args['room'] if 'room' in request.args else None
+    # room=request.args['room'] if 'room' in request.args else None
 
     db = get_db()
-    image = db.projects.find_one({'_id': "%s%s" % (user, project)}, {'images':{'$slice':-1}})
+    image = db.projects.find_one({'_id': "%s%s" % (user, project)}, {'images': {'$slice': -1}})
     if image and 'images' in image and image['images']:
         GFS = get_gridfs()
         img = GFS.get(image['images'][0])
@@ -116,9 +122,8 @@ def project(user, project):
     else:
         img = None
 
-
-
     return render_template('project.html', project=project, img=img)
+
 
 # @project_B.route('/imgupdate', methods = ['POST'])
 # def imgupdate():
@@ -143,29 +148,29 @@ def imgupdate():
 def imgupdate(update, project, user):
     print("imgpush")
 
-
     # if not all([x in request.form for x in ('project', 'user')]):
     #     return jsonify({'ok': 0})
     # join_room(session['user'])
     # room = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
 
-    emit('imgupdate', {'update': update, 'user':user}, room=project)
+    emit('imgupdate', {'update': update, 'user': user}, room=project)
 
-@project_B.route('/getImgUpdates', methods = ['POST'])
+
+@project_B.route('/getImgUpdates', methods=['POST'])
 def getImgUpdates():
-    #room=request.args['room'] if 'room' in request.args else None
+    # room=request.args['room'] if 'room' in request.args else None
 
     print(request.form['update'])
 
     if not all([x in request.form for x in ('project', 'user')]):
-        return jsonify({'ok':0})
+        return jsonify({'ok': 0})
 
-    emit('imgupdate', {'update': request.form['update'], 'user':request.form['user']}, room=request.form['project'])
+    emit('imgupdate', {'update': request.form['update'], 'user': request.form['user']}, room=request.form['project'])
 
     return jsonify({'ok': 0})
 
 
-@project_B.route('/changeUserName', methods = ['POST', 'GET'])
+@project_B.route('/changeUserName', methods=['POST', 'GET'])
 def changeUserName():
     if all(x in request.form for x in ('g-recaptcha-response', 'userNameChange')):
 
@@ -182,25 +187,24 @@ def changeUserName():
     else:
         return jsonify({'ok': 0})
 
-labels={
-            'US_News': 'US News',
-            'World_News':'World News',
-            'Politics':'Politics',
 
-            'Technology':'Technology',
-            'Entertainment':'Entertainment',
-            'Business':'Business',
+labels = {
+    'US_News': 'US News',
+    'World_News': 'World News',
+    'Politics': 'Politics',
 
-            'Sports':'Sports',
+    'Technology': 'Technology',
+    'Entertainment': 'Entertainment',
+    'Business': 'Business',
 
-            'Memes':'Memes'}
+    'Sports': 'Sports',
 
-@project_B.route('/categories/<category>', methods = ['GET'])
+    'Memes': 'Memes'}
+
+
+@project_B.route('/categories/<category>', methods=['GET'])
 def categories(category):
-
     name = labels[category]
-
-
 
     # getting the time until switch
     interval = app.config['SWITCH_SECONDS']
@@ -209,30 +213,27 @@ def categories(category):
     remainingTime = interval - (current_time % interval)
     print(remainingTime)
 
-
-
-
     return render_template('categories.html', categoryName=name,
                            category=category,
                            remainingTime=remainingTime,
                            interval=interval)
 
-@project_B.route('/testArticles', methods = ['GET'])
-def testArticles():
 
+@project_B.route('/testArticles', methods=['GET'])
+def testArticles():
     db = get_db()
     for i in range(30):
-
-        db.articles.insert({'url':'http://stackoverflow.com/questions/%d' % (5584586+i),
-                            'title':'test%d' % (5584586+i),
-                            'category':'Technology'})
+        db.articles.insert({'url': 'http://stackoverflow.com/questions/%d' % (5584586 + i),
+                            'title': 'test%d' % (5584586 + i),
+                            'category': 'Technology'})
 
     return "OK"
 
-@project_B.route('/getCurrentArticle', methods = ['POST'])
+
+@project_B.route('/getCurrentArticle', methods=['POST'])
 def getCurrentArticle():
     if not 'category' in request.json:
-        return jsonify({'ok':0, 'err':'Must provide category'})
+        return jsonify({'ok': 0, 'err': 'Must provide category'})
 
     interval = app.config['SWITCH_SECONDS']
 
@@ -240,38 +241,39 @@ def getCurrentArticle():
     intervals_passed = current_time // interval
 
     db = get_db()
-    article = db.articles.find({'category': request.json['category']}).sort([('$natural',1)]).limit(1)
+    article = db.articles.find({'category': request.json['category']}).sort([('$natural', 1)]).limit(1)
     if not article.count():
         return jsonify({'ok': 0, 'err': 'no articles!'})
     article = article[0]
-    #print article
+    # print article
 
     if 'index' not in article:
         db.articles.update({'_id': article['_id']}, {'$set': {'index': intervals_passed}})
 
     elif 'index' in article and intervals_passed > article['index']:
-        db.articles.remove({'_id':article['_id']})
-        article = db.articles.find({'category': request.json['category']}).sort([('$natural',1)]).limit(1)
+        db.articles.remove({'_id': article['_id']})
+        article = db.articles.find({'category': request.json['category']}).sort([('$natural', 1)]).limit(1)
         if not article.count():
             return jsonify({'ok': 0, 'err': 'no articles!'})
         article = article[0]
         db.articles.update({'_id': article['_id']}, {'$set': {'index': intervals_passed}})
 
     del article['_id']
-    return jsonify({'ok':1, 'article':article})
+    return jsonify({'ok': 1, 'article': article})
 
-@project_B.route('/upload', methods = ['POST'])
+
+@project_B.route('/upload', methods=['POST'])
 def upload():
     if not 'category' in request.json or not 'url' in request.json or not 'title' in request.json:
-        return jsonify({'ok':0, 'err':'Must provide category'})
+        return jsonify({'ok': 0, 'err': 'Must provide category'})
 
     category = request.json['category']
     url = request.json['url']
     title = request.json['title']
     if category and url and title:
-        article = {'url':url,
-                   'title':title,
-                   'category':category}
+        article = {'url': url,
+                   'title': title,
+                   'category': category}
         try:
             if 'title' not in article or not article['title'] or article['title'].isspace():
                 article['title'] = getArticle.get_generic_title(article['url'])
@@ -292,8 +294,7 @@ def upload():
         except Exception as e:
             print("unknown error: %s" % str(e))
 
-    return jsonify({'ok':1})
-
+    return jsonify({'ok': 1})
 
 
 @socketio.on('connect')
@@ -304,6 +305,7 @@ def connect3():
 
     emit('connectConfirm')
 
+
 @socketio.on('connect', namespace='/chat')
 def connect2():
     # join_room(session['user'])
@@ -311,40 +313,43 @@ def connect2():
 
     emit('connectConfirm')
 
+
 # 'disconnected' is a special event
 @socketio.on('disconnect')
 def disconnected():
     pass
 
+
 @socketio.on('userDisconnect', namespace='/chat')
 def userDisconnect(user, room):
-    #db = get_db()
-    #db.msgs.insert({'room':room, 'user':user, 'left':True, 'time':tStamp})
+    # db = get_db()
+    # db.msgs.insert({'room':room, 'user':user, 'left':True, 'time':tStamp})
 
     leave_room(room)
-    emit('userDisconnect', {'ok': 1, 'user':user, 'room':room}, room=room)
+    emit('userDisconnect', {'ok': 1, 'user': user, 'room': room}, room=room)
+
 
 @socketio.on('requestUpdate', namespace='/chat')
 def getUpdate(project, layer):
     print('fsaf')
-    emit('imageUpdate', {'ok':1, 'updates':[(1, 2, 244, 231, 5),
-                                            (2, 1, 24, 231, 5),
-                                            (3, 9, 244, 21, 5),
-                                            (0, 2, 24, 231, 25),]},
-                                            room=project)
+    emit('imageUpdate', {'ok': 1, 'updates': [(1, 2, 244, 231, 5),
+                                              (2, 1, 24, 231, 5),
+                                              (3, 9, 244, 21, 5),
+                                              (0, 2, 24, 231, 25), ]},
+         room=project)
 
 
 @socketio.on('joined', namespace='/chat')
 def joined(user, project):
     join_room(project)
-    #at some point, twisted will be implemented instead of this
-    #loggedIn.addUser(user)
-    #print loggedIn.getUsers()
-    #db = get_db()
-    #db.msgs.insert({'room':room, 'user':user, 'joined':True, 'time':tStamp})
+    # at some point, twisted will be implemented instead of this
+    # loggedIn.addUser(user)
+    # print loggedIn.getUsers()
+    # db = get_db()
+    # db.msgs.insert({'room':room, 'user':user, 'joined':True, 'time':tStamp})
 
     print('user joined')
-    emit('joined', {'ok': 1, 'user': user, 'room':project}, room=project)
+    emit('joined', {'ok': 1, 'user': user, 'room': project}, room=project)
 
 
 # @socketio.on('checkUsersOnlineInit', namespace='/chat')
@@ -360,14 +365,12 @@ def joined(user, project):
 
 @socketio.on('chatMsg', namespace='/chat')
 def chatMsg(user, room, data):
-
-
     # #msgs.insert({'post':message})
     # if message != 'connected':
     #
     #     msgsCol.insert({'user':message['user'], 'msg':message['post']})
     #
     #     emit('update', message, broadcast=True)
-    #db = get_db()
-    #db.msgs.insert({'room':room, 'user':user, 'msg':data})
-    emit('chatMsg', {'user':user, 'data':data, 'room':room}, room=room)
+    # db = get_db()
+    # db.msgs.insert({'room':room, 'user':user, 'msg':data})
+    emit('chatMsg', {'user': user, 'data': data, 'room': room}, room=room)
---
+++ app/picture_change.py
@@ -0,0 +1,78 @@
+from PIL import Image
+from PIL import ImageChops
+from pprint import pprint
+
+def getChanges():
+    im_old = Image.open()
+    im_old = im_old.convert("RGBA")
+    im_new = Image.open('/home/goods/Desktop/batnew.png')
+    im_new = im_new.convert("RGBA")
+    pixels_old = im_old.load() # this is not a list, nor is it list()'able
+    pixels_new = im_new.load()
+    pixelToPng = []
+    picChange = False
+    removedPixel = []
+    addedPixel = []
+    finalChangeIm = []
+    transparent = (255,255,255,0)
+    numPixelChange=0
+    width, height = im_old.size
+    width, height = im_new.size
+    all_pixelsOldLst = []
+    finalGrab= []
+    all_pixelsOld = []
+    all_pixelsNew = []
+    rafDictionary={}
+
+    for x in range(width):
+        for y in range(height):
+            cpixel_old = pixels_old[x, y]
+            pixAndLoc = cpixel_old + (x,) + (y,)
+            all_pixelsOld.append(pixAndLoc)
+
+
+    for x in range(width):
+        for y in range(height):
+            cpixel_new = pixels_new[x, y]
+            pixAndLocNew = cpixel_new + (x,) + (y,)
+            all_pixelsNew.append(pixAndLocNew)
+
+
+    for i in range(len(all_pixelsOld)):
+        if(all_pixelsOld[i]!=all_pixelsNew[i]):
+            picChange=True
+            removedPixel.append(all_pixelsOld[i])
+            addedPixel.append(all_pixelsNew[i])
+            rafDictionary['r']=removedPixel
+            rafDictionary['a']=addedPixel
+
+    #print(removedPixel)
+    #print(addedPixel)
+
+    changedPixImage = Image.new(im_new.mode, im_new.size )
+    #changedPixImage.load()
+    for x in range(width):
+        for y in range(height):
+            for i in addedPixel:
+                if i[4]==x and i[5]==y:
+                    tmpLoc= i[4],+i[5]
+                    tmpPix = i[0],+i[1],+i[2],+i[3],
+                    tmpFull = tmpPix + tmpLoc
+                    changedPixImage.putpixel(tmpLoc,tmpPix)
+                    finalChangeIm.append(tmpFull)
+                    addedPixel.remove(i)
+                    rafDictionary['d']=finalChangeIm
+                    break
+                else:
+                    tmpXY = x,+y
+                    changedPixImage.putpixel(tmpXY,transparent)
+                    finalChangeIm.append(transparent)
+                    rafDictionary['d']=finalChangeIm
+
+                    break
+
+    pprint(rafDictionary)
+    #changedPixImage.show()
+    #changedPixImage.save('/home/goods/Desktop/changedPixImage.png')
+    return rafDictionary
+getChanges()
